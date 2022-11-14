77f3ccb5386e0e0363098251369d7bcc60346d8c
raincbc <raincbc@gmail.com>
2022-11-14 21:12:23 +0200

    start second task

--- h2/src/index.js
+++ h2/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import  FirstTask from './tasks/FirstTask.js'
+import FirstTask from './tasks/FirstTask.js'
+import UserForm from './tasks/SecondTask.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <UserForm/>
     <FirstTask/>
   </React.StrictMode>
 );
--- h2/src/tasks/SecondTask.js
+++ h2/src/tasks/SecondTask.js
@@ -3,5 +3,46 @@
 import React from "react";
 
 export default class UserForm extends React.Component{
-    
+    state = {
+        name: '',
+        surname: '',
+        error:''
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    handleChange = (event) => {
+    }
+
+    changeValue = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <input
+                            placeholder="name"
+                            name="name"
+                            onChange={this.changeValue}
+                            value={this.state.name}></input>
+                    </div>
+                    <div>
+                        <input
+                            placeholder="surname"
+                            name="surname"
+                            onChange={this.changeValue}
+                            value={this.state.surname}></input>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
 }

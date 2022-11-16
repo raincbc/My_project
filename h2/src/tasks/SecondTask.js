42778c76d61ff42b8ab2bbaa615ddc6f3d1be40e
raincbc <raincbc@gmail.com>
2022-11-16 18:13:27 +0200

    change h2

--- h2/src/tasks/SecondTask.js
+++ h2/src/tasks/SecondTask.js
@@ -11,6 +11,17 @@ export default class UserForm extends React.Component{
 
     handleSubmit = (event) => {
         event.preventDefault();
+        
+        if (!this.state.name) {
+            this.setState({error:'You don\'t enter name'})
+        } else if (!this.state.surname) {
+            this.setState({error:'You don\'t enter surname'})
+        } else {
+            
+            alert(`Добро пожаловать ${this.state.surname} ${this.state.name}`)
+            event.target.name.value = '';
+            event.target.surname.value = '';
+        }
     };
 
     handleChange = (event) => {
@@ -40,7 +51,8 @@ export default class UserForm extends React.Component{
                             onChange={this.changeValue}
                             value={this.state.surname}></input>
                     </div>
-                    <button type="submit">Submit</button>
+                    <span name="error">{ this.state.error}</span>
+                    <button type="submit" >Submit</button>
                 </form>
             </div>
         )

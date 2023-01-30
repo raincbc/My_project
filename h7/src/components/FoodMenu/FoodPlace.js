7ac9b2eb029bea4651603530b49778886eebf151
raincbc <raincbc@gmail.com>
2023-01-30 09:21:06 +0200

    change

--- h7/src/components/FoodMenu/FoodPlace.js
+++ h7/src/components/FoodMenu/FoodPlace.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import styled from 'styled-components'
 import { FoodContext } from '../../context/context'
 import FoodPlaceTitle from './FoodPlaceTitle'
